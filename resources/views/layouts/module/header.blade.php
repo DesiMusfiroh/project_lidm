2e192d2c5e6e0923b029c344de744d9b87a14a54
DesiMusfiroh <musfiroh.desi@gmail.com>
2020-07-24 18:05:35 +0700

    make a new project

---
+++ resources/views/layouts/module/header.blade.php
@@ -0,0 +1,32 @@
+<header class="app-header navbar">
+    <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+        <img class="navbar-brand-full" src="" width="93"  alt="U-Market">
+        <img class="navbar-brand-minimized" src="" width="50"  alt="U-market Logo">
+    </a>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="nav navbar-nav ml-auto">
+        
+        <li class="nav-item d-md-down-none">
+            <a class="nav-link" href="#">
+       
+        </a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <img class="img-avatar" src="" alt="">
+            </a>
+        <div class="dropdown-menu dropdown-menu-right">
+            <div class="dropdown-header text-center">
+                <strong>Account</strong>
+            </div>
+            <div class="divider"></div>
+           
+        </div>
+        </li>
+    </ul>
+</header>
---
+++ resources/views/tes.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('title')
+    <title>List Product</title>
+@endsection
+
+@section('content')
+<main class="main">
+    <ol class="breadcrumb bg-dark">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item active">Product</li>
+    </ol>
+    <div class="container-fluid">
+        
+    </div>
+</main>
+@endsection
