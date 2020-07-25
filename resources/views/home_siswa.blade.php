831bd123ba2658024cc241bd107926252e571b4c
krismandev <krismanpratama@gmail.com>
2020-07-25 22:03:58 +0700

    menyamakan kode

--- resources/views/home_siswa.blade.php
+++ resources/views/home_siswa.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
 <main class="main">
-    <ol class="breadcrumb bg-dark">
+    <ol class="breadcrumb bg-white">
         <li class="breadcrumb-item">Home</li>
         <li class="breadcrumb-item active">Siswa</li>
     </ol>
