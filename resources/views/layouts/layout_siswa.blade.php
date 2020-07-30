a451174a997a6c8bf6ac8142df24d9c0423ee4b8
krismandev <krismanpratama@gmail.com>
2020-07-30 20:28:31 +0700

    menyamakan kode

--- resources/views/layouts/layout_siswa.blade.php
+++ resources/views/layouts/layout_siswa.blade.php
@@ -356,7 +356,8 @@
                 <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
         </div>
     </div>
-<script type="text/javascript" src="{{asset('/assets')}}/scripts/main.js"></script></body>
+<script type="text/javascript" src="{{asset('/assets')}}/scripts/main.js"></script>
+</body>
 @yield('js')
 @yield('chart')
 </html>
