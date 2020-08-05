7da75491e011ad74f640a077ba6ffb693fd41e7e
krismandev <krismanpratama@gmail.com>
2020-08-06 00:23:29 +0700

    menyamakan kode

--- resources/views/Ujian-Siswa/index.blade.php
+++ resources/views/Ujian-Siswa/index.blade.php
@@ -6,62 +6,61 @@
 <div>
   {{ Breadcrumbs::render('siswa.ujian.index') }}
 </div>
-<div class="container-fluid">
-    @if($peserta_ujian->count() != 0)
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <strong style="font-size:18px;">Ujian yang akan dikerjakan</strong>
-                </div>
-                <div class="card-body">
-                    <div class="table-inside">
-                        <table class="table table-striped table-sm">
-                            <thead class="text-center"  style="background-color:#393A3C; color:white; font-weight:bold">
-                                <tr>
-                                    <th scope="col" style="width:50px">No</th>
-                                    <th scope="col" >Nama Ujian </th>
-                                    <th scope="col" >Waktu Mulai </th>
-                                    <th scope="col" >Durasi </th>
-                                    <th scope="col" >Keterangan </th>
-                                    <th scope="col" style="width:100px">Detail </th>
+<div class="">
+		@if($peserta_ujian->count() != 0)
+		<div class="row">
+			@foreach($peserta_ujian as $item)
+			<div class="col-md-4">
+				<a href="{{route('waitUjian',$item->id)}}">
+			    <div class="card mb-3 widget-content">
+			        <div class="widget-content-wrapper text-white">
+			            <div class="widget-content-left">
+			                <div class="widget-heading text-white"><span> {{$item->ujian->nama_ujian}}</span></div>
+											<div class="widget-heading text-white" title="Waktu mulai ujian">{{date('d M Y, H:i',strtotime($item->ujian->waktu_mulai))}}</div>
+											<div class="widget-subheading text-white">
+												<span>
+													<?php
+													if ($item->ujian->status == 0) {
+															echo "ujian segera dimulai";
+													} elseif ($item->ujian->status == 1) {
+															echo "ujian sedang berlangsung";
+													} elseif ($item->ujian->status == 2) {
+															echo "ujian telah berakhir";
+													}
+													?>
+												</span>
+											</div>
+			            </div>
+			            <div class="widget-content-right">
 
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php $i=0; ?>
-                                @foreach ($peserta_ujian as $item)
-                                <tr>
-                                    <td scope="row" class="text-center"><?php  $i++;  echo $i; ?></td>
-                                    <td > {{$item->ujian->nama_ujian}}</td>
-                                    <td class="text-center">{{$item->ujian->waktu_mulai}} </td>
-                                    <td class="text-center">{{$item->ujian->paket_soal->durasi}} </td>
-                                    <td class="text-center">
-                                        <?php
-                                        if ($item->ujian->status == 0) {
-                                            echo "ujian segera dimulai";
-                                        } elseif ($item->ujian->status == 1) {
-                                            echo "ujian sedang berlangsung";
-                                        } elseif ($item->ujian->status == 2) {
-                                            echo "ujian telah berakhir";
-                                        }
-                                        ?>
-                                    </td>
-                                    <td class="text-center"><a href="{{route('waitUjian',$item->id)}}">
-                                        <button type="button" class="btn btn-warning btn-sm">
-                                                <i class="fa fa-edit fa-sm"></i> Masuk
-                                            </button> </a>
-                                    </td>
+			            </div>
+			        </div>
+			    </div>
+				</a>
+			</div>
+		@endforeach
+		</div>
+		@else
+		<div class="col-md-12">
+				<div class="alert alert-warning" role="alert">
+						Tidak ada ujian yang akan diikuti
+				</div>
+		</div>
+		@endif
+	</div>
 
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    @endif
-</div>
+	<script>
+	  $(document).ready(function() {
+	    var r = Math.round(Math.random() * 255 + 1);
+	    var g = Math.round(Math.random() * 255 + 1);
+	    var b = Math.round(Math.random() * 255 + 1);
+			console.log(r,g,b);
+	    // var card = document.querySelector('.card');
+			$('.card').css({
+				'backgroundImage' :'linear-gradient(to right, rgb('+r+','+g+','+b+'), rgb('+r+','+g+','+b+'),rgb('+r+','+g+','+b+')'
+			});
+	  });
+		// linear-gradient(to right, #4682B4, #00FFFF, #00FA9A);
+		// 'linear-gradient(to right, rgb('+r+','+g+','+b+'), rgb('+r+','+g+','+b+'),rgb('+r+','+g+','+b+')'
+	</script>
 @endsection
